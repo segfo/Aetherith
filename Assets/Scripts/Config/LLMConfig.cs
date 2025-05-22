0c10456ecdf68f0f926de3c1035e42f95851683d
segfo <k.segfo@gmail.com>
2025-05-22 20:57:46 +0900

    :sparkles::bug: TOP_KとTOP_Pの実装+軽微なバグ修正
    TOP_KとTOP_Pの実装を行いました。
    また、キャラクタLLMのtemperatureが感情推定LLMに使用されていたため修正しました。

--- Assets/Scripts/Config/LLMConfig.cs
+++ Assets/Scripts/Config/LLMConfig.cs
@@ -6,19 +6,15 @@ public class LLMConfig
     public string difyApiUrl = "";
     // ローカルのLLMを使う場合はモデル名を指定する(ggufファイル)
     public string modelName = "gemma3.gguf";
-    public string llmEmotionModelName = "gemma3.gguf";
     // DifyのAPIを使う場合はAPIキーを指定する
     public string difyApiKey = "";
     // ローカルLLM（実応答）システムプロンプトの記述されたファイル名
-    public string agentSystemPromptFile = "systemprompt.txt";
+    public string systemPromptFile = "systemprompt.txt";
     // ローカルLLM（感情判定）システムプロンプトの記述されたファイル名
-    public string emotionSystemPromptFile = "emotion_systemprompt.txt";
     public string userName = "User";
     public string assistantName = "Assistant";
     public int maxContextLength = 8192;
-    public int llmEmotionMaxContextLength=8192;
-    public string welcomeMessage = "何でも聞いてくださいね！";
-    public string waitMessage = "\"（考え中です…）\"";
-    public double temperature = 0.7f;
-    public double emotionalTemperature = 0.0f;
+    public float temperature = 0.7f;
+    public float topP = 0.95f;
+    public int topK = 40;
 }
