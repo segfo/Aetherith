58da73a8d7286c9ce603be9d79c4ca9b4d009995
segfo <k.segfo@gmail.com>
2025-05-22 21:41:43 +0900

    :recycle: 設定ファイルのリファクタリング
    - キャラクタLLMと感情推定LLMの設定項目を分離した。
    - VrmDisplayOffsetメンバ（X・Y）の順序を入れ替えた。

--- Assets/Scripts/Config/AppConfig.cs
+++ Assets/Scripts/Config/AppConfig.cs
@@ -2,7 +2,7 @@
 public class AppConfig
 {
     public LLMConfig characterLlm = new LLMConfig();
-    public LLMConfig emotionLlm = new LLMConfig();
+    public LLMConfig emotionLlm = new LLMConfig("emotion_systemprompt.txt",0.0f);
     public VRMConfig vrm = new VRMConfig();
     public string welcomeMessage = "何でも聞いてくださいね！";
     public string waitMessage = "\"（考え中です…）\"";
--- Assets/Scripts/Config/VRMConfig.cs
+++ Assets/Scripts/Config/VRMConfig.cs
@@ -2,15 +2,15 @@
 public class VRMConfig
 {
     public int ToneMappingMode = 0; // 0: None, 1: Neutral, 2: ACES
-    public float LightIntensity = 0.60000000f;
-    public string LightColorRGBA = "#FFF0E0FF";
-    public float ShadowStrength = 0.3000000f;
-    public float Scale = 1.0f;
-    public float VrmDisplayOffsetY = 0.15f;
-    public float VrmDisplayOffsetX = -1.5f;
-    public bool BackgroundWindowTransparent = true;
-    public string FileName = "Default.vrm";
-    public SpringBone springBone = new SpringBone();
+    public float LightIntensity = 1f;  // 明るさ: 0.0f ～ 1.0f
+    public string LightColorRGBA = "#FFF0E0FF";  // ライトの色（肌が白くなりすぎないように）
+    public float ShadowStrength = 0.3000000f; // 影の強さ: 0.0f ～ 1.0f
+    public float Scale = 1.0f;  // VRMのスケール
+    public float VrmDisplayOffsetX = -1.5f; // VRMの表示位置調整 X軸
+    public float VrmDisplayOffsetY = 0.15f; // VRMの表示位置調整 Y軸
+    public bool BackgroundWindowTransparent = true; // 背景ウィンドウを透明にする
+    public string FileName = "Default.vrm"; // VRMファイル名
+    public SpringBone springBone = new SpringBone(); // SpringBone = 揺れ物の設定
 }
 public class SpringBone
 {
