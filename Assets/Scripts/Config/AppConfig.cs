8926ea52461ce3033aea55fadca51e36c7ec29c9
segfo <k.segfo@gmail.com>
2025-05-22 20:57:46 +0900

    :sparkles::bug: TOP_KとTOP_Pの実装+軽微なバグ修正
    TOP_KとTOP_Pの実装を行いました。
    また、キャラクタLLMのtemperatureが感情推定LLMに使用されていたため修正しました。

--- Assets/Scripts/Config/AppConfig.cs
+++ Assets/Scripts/Config/AppConfig.cs
@@ -1,6 +1,9 @@
 ﻿[System.Serializable]
 public class AppConfig
 {
-    public LLMConfig llm = new LLMConfig();
+    public LLMConfig characterLlm = new LLMConfig();
+    public LLMConfig emotionLlm = new LLMConfig();
     public VRMConfig vrm = new VRMConfig();
+    public string welcomeMessage = "何でも聞いてくださいね！";
+    public string waitMessage = "\"（考え中です…）\"";
 }
