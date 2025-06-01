339b7080a61bf1f2b62f75316410b571066529eb
segfo <k.segfo@gmail.com>
2025-06-01 16:53:30 +0900

    コメント追加

--- Assets/Scripts/UI/ChatManager.cs
+++ Assets/Scripts/UI/ChatManager.cs
@@ -71,6 +71,7 @@ public class ChatManager : MonoBehaviour
         }
         else
         {
+            // 共通のLLMを使う。
             llmEmotional = llm;
             llmCharacterEmotional = characterLLM.AddComponent<LLMCharacter>();
         }
